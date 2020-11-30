8ac147931f4e4274b07c34b5454b7893fc0cd6b9
Francisco Rosado <Franciscorosado09@gmail.com>
2020-11-29 21:00:47 -0500

    update html, css, js

---
+++ style.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+var button = document.getElementsByClassName("btn btn-primary btn-lg")
+var time = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+
+// var secondsLeft = 100;
+
+// function setTime() {
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+//     if(secondsLeft === 0) {
+//       clearInterval(timerInterval);
+//       sendMessage();
+//     }
+
+//   }, 1000);
+}
